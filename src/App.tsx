6e657e7fa201cbacacf7c2558d11af6c09869d20
Benson Kiai <benson.kiai86@gmail.com>
2024-01-26 23:50:49 +0300

    Update logo in App component

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,20 @@ function App() {
       <div className="grid lg:grid-cols-2 2xl:grid-cols-5 ">
         <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
           <div className="xl:max-w-xl ">
-            <img className="h-10" src="src/assets/logo.svg" alt="Workcation" />
+            <img
+              className="h-10"
+              src="src/assets/logo-brand.svg"
+              alt="Workcation"
+            />
             <img
               className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover sm:object-center lg:hidden"
               src="src/assets/beach-work.jpg"
               alt="Woman workcationing on the beach"
             />
-            <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl ">
+            <h1 className="mt-6 text-2xl font-headline tracking-tighter font-semibold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl ">
               You can work from anywhere.
               <br className="inline" />
-              <span className="text-indigo-500">Take advantage of it.</span>
+              <span className="text-brand">Take advantage of it.</span>
             </h1>
             <p className="mt-2 text-gray-600 sm:mt-4 sm: text-xl">
               Workcation helps you find work-friendly rentals in beautiful
